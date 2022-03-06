5dc0df4b1588dc90c17606046e86575c69e1112d
dennis <denizponce16@gmail.com>
2022-03-06 10:26:50 -0500

    first commit

---
+++ api/app.js
@@ -0,0 +1,29 @@
+// Create server Express
+
+// Define endpoint for ToDos
+// GET fetch all ToDos
+// POST Create new ToDo
+// PATCH Update ToDo given an ID
+// DELETE Delete ToDo given an ID (destroy or soft delete)
+
+// Establish a connection with a Database (Postgress)
+
+// Create ToDo model
+// Use the model to interact with the controller functions
+
+// Must structure project with routes, controllers and models folders (utils)
+
+// IMPORTANT: Prettier format
+
+// Install cors library (npm i cors)
+// app.use(cors())
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(4000, () => {
+  console.log('API running');
+});
